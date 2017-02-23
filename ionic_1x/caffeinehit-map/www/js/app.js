d1dff7546874b69615543e62ac4b89f04967f46c
Hasan Kumar <mintuhouse@gmail.com>
2017-02-24 03:33:59 +0530

    Base app

---
+++ ionic_1x/caffeinehit-map/www/js/app.js
@@ -0,0 +1,26 @@
+var app = angular.module('caffeinehit', [
+	'ionic',
+	'caffeinehit.controllers',
+	'caffeinehit.services',
+	'caffeinehit.filters'
+]);
+
+app.run(function ($ionicPlatform) {
+	$ionicPlatform.ready(function () {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.disableScroll(true);
+
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
+});
+
+app.config(function ($httpProvider) {
+	$httpProvider.defaults.headers.common['Authorization'] = 'Token 54a45b79c2927d318205ff96e5dfa4dfc1e46627';
+});
