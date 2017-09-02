30de43378633a012aec8f0e58aadf4ed1c00d16b
Hasan Kumar <mintuhouse@gmail.com>
2017-09-02 07:49:53 +0530

    Simple Node Web Server

--- nodejs/node-web-server/server.js
+++ nodejs/node-web-server/server.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.send('Hello Express!');
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is up on 3000');
+});
