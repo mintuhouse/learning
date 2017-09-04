1765cd16c9e5e313d3fc1faa5d886cafc8a77cf8
Hasan Kumar <mintuhouse@gmail.com>
2017-09-04 21:42:26 +0530

    POST /todos with tests

--- nodejs/node-todo-api/server/server.js
+++ nodejs/node-todo-api/server/server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var {
   mongoose
@@ -30,3 +30,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
