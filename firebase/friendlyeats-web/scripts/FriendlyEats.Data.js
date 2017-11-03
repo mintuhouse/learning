6ab6fcb7f6fa4ac201019fb448094df771f5e3fc
Hasan Kumar <mintuhouse@gmail.com>
2017-11-03 16:06:38 +0530

    Add base project for firestore codelab

--- firebase/friendlyeats-web/scripts/FriendlyEats.Data.js
+++ firebase/friendlyeats-web/scripts/FriendlyEats.Data.js
@@ -15,37 +15,59 @@
  */
 'use strict';
 
-FriendlyEats.prototype.addRestaurant = function(data) {
-  /*
-    TODO: Implement adding a document
-  */
+FriendlyEats.prototype.addRestaurant = function (data) {
+  const collection = firebase.firestore().collection('restaurants');
+  return collection.add(data);
 };
 
-FriendlyEats.prototype.getAllRestaurants = function(render) {
-  /*
-    TODO: Retrieve list of restaurants
-  */
+FriendlyEats.prototype.getAllRestaurants = function (render) {
+  const query = firebase.firestore()
+    .collection('restaurants')
+    .orderBy('avgRating', 'desc')
+    .limit(50)
+  this.getDocumentsInQuery(query, render);
 };
 
-FriendlyEats.prototype.getDocumentsInQuery = function(query, render) {
-  /*
-    TODO: Render all documents in the provided query
-  */
+FriendlyEats.prototype.getDocumentsInQuery = function (query, render) {
+  query.onSnapshot(snapshot => {
+    if (!snapshot.size) return render();
+
+    snapshot.docChanges.forEach(change => {
+      if (change.type === 'added') {
+        render(change.doc);
+      }
+    });
+  });
 };
 
-FriendlyEats.prototype.getRestaurant = function(id) {
-  /*
-    TODO: Retrieve a single restaurant
-  */
+FriendlyEats.prototype.getRestaurant = function (id) {
+  return firebase.firestore().collection('restaurants').doc(id).get();
 };
 
-FriendlyEats.prototype.getFilteredRestaurants = function(filters, render) {
-  /*
-    TODO: Retrieve filtered list of restaurants
-  */
+FriendlyEats.prototype.getFilteredRestaurants = function (filters, render) {
+  let query = firebase.firestore().collection('restaurants');
+  if (filters.category !== 'Any') {
+    query = query.where('category', '==', filters.category);
+  }
+
+  if (filters.city !== 'Any') {
+    query = query.where('city', '==', filters.city);
+  }
+
+  if (filters.price !== 'Any') {
+    query = query.where('price', '==', filters.price);
+  }
+
+  if (filters.sort === 'Rating') {
+    query = query.orderBy('avgRating', 'desc');
+  } else if (filters.sort === 'Reviews'){
+    query = query.orderBy('numRatings', 'desc');
+  }
+
+  this.getDocumentsInQuery(query, render);
 };
 
-FriendlyEats.prototype.addRating = function(restaurantID, rating) {
+FriendlyEats.prototype.addRating = function (restaurantID, rating) {
   /*
     TODO: Retrieve add a rating to a restaurant
   */
